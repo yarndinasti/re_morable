e961e25ced211b47bccfba1736105b6af324caed
yarndinasti <ian.ian.wold76@gmail.com>
2022-04-06 19:24:41 +0700

    add double exit

--- lib/components/list_video.dart
+++ lib/components/list_video.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:re_morable/modules/anchor.dart';
+import 'package:re_morable/modules/home_model.dart';
 
 class ListVideo extends StatefulWidget {
-  // ignore: prefer_typing_uninitialized_variables
-  final list;
+  final List<Videos> list;
   const ListVideo({Key? key, required this.list}) : super(key: key);
 
   @override
@@ -11,35 +11,11 @@ class ListVideo extends StatefulWidget {
 }
 
 class _ListVideoState extends State<ListVideo> {
-  List<Widget> theList = [];
   @override
-  void initState() {
-    super.initState();
-    setState(() {
-      //looping widget.list
-      for (var i = 0; i < widget.list.length; i++) {
-        theList.add(
-          ListTile(
-            title: Text(widget.list[i]['title']),
-            subtitle: Text(widget.list[i]['member']['name']),
-            leading: Image.network(
-              widget.list[i]['mini_thumbnail'],
-              fit: BoxFit.cover,
-              width: 90,
-              height: 90 / 16 * 9,
-            ),
-            onTap: () => launchURL(
-                'https://youtube.com/watch?v=${widget.list[i]['id']}'),
-          ),
-        );
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: theList,
+      children: [Container()],
     );
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -6,6 +6,7 @@ import 'package:re_morable/modules/slideshow_loading.dart';
 import 'package:re_morable/modules/home_model.dart';
 import 'package:re_morable/modules/fetch.dart';
 import 'package:re_morable/modules/save_local.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,6 +16,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  DateTime? preExit;
+
   @override
   initState() {
     super.initState();
@@ -53,77 +56,101 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-        triggerMode: RefreshIndicatorTriggerMode.onEdge,
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          addAutomaticKeepAlives: true,
-          children: <Widget>[
-            // when isError is true, hide slideshow
-            SafeArea(
-                child: (SaveLocal.data == null)
-                    ? const SlideshowLoading()
-                    : SlideshowHome(tabSlideshow: SaveLocal.data!.slideshow)),
-            // make header text
-            Container(
-              margin: const EdgeInsets.only(top: 10, bottom: 10, left: 10),
-              alignment: Alignment.topLeft,
-              child: const Text(
-                "Members",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
+    return WillPopScope(
+      onWillPop: () async {
+        final initApp = DateTime.now().subtract(const Duration(seconds: 10));
+
+        final timegap =
+            DateTime.now().difference(preExit != null ? preExit! : initApp);
+
+        final onExit = timegap < const Duration(seconds: 5);
+        if (onExit) return true;
+
+        setState(() {
+          preExit = DateTime.now();
+        });
+
+        // add toast in botom screen
+        Fluttertoast.showToast(
+          msg: "Mau keluar? Tekan/swipe back lagi!",
+          toastLength: Toast.LENGTH_LONG,
+          timeInSecForIosWeb: 5,
+        );
+
+        return false;
+      },
+      child: RefreshIndicator(
+          triggerMode: RefreshIndicatorTriggerMode.onEdge,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            addAutomaticKeepAlives: true,
+            children: <Widget>[
+              // when isError is true, hide slideshow
+              SafeArea(
+                  child: (SaveLocal.data == null)
+                      ? const SlideshowLoading()
+                      : SlideshowHome(tabSlideshow: SaveLocal.data!.slideshow)),
+              // make header text
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 10, left: 10),
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  "Members",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
                 ),
               ),
-            ),
-            // create list item flex row
-            const MemberList(),
-            // make header text
-            Container(
-              margin: const EdgeInsets.only(top: 10, bottom: 10, left: 10),
-              alignment: Alignment.topLeft,
-              child: (SaveLocal.isError)
-                  ? Container()
-                  : const Text(
-                      "Latest Videos",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black,
+              // create list item flex row
+              const MemberList(),
+              // make header text
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 10, left: 10),
+                alignment: Alignment.topLeft,
+                child: (SaveLocal.isError)
+                    ? Container()
+                    : const Text(
+                        "Latest Videos",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-            ),
-            //(isError) ? Container() : ListVideo(list: data!['list']),
-          ],
-        ),
-        onRefresh: () async {
-          //sleep 5000
-          final _result = await RemoteService.getData(
-              "http://rem-play-server.yansaan.repl.co/on-app");
-
-          if (_result == null) {
-            setState(() {
-              SaveLocal.isError = true;
-            });
-            // add message
-            final snackBar = SnackBar(
-              content: const Text("Check your internet connection lol"),
-              action: SnackBarAction(
-                label: "OK",
-                // ignore: avoid_returning_null_for_void
-                onPressed: () => null,
               ),
-            );
+              //(isError) ? Container() : ListVideo(list: data!['list']),
+            ],
+          ),
+          onRefresh: () async {
+            //sleep 5000
+            final _result = await RemoteService.getData(
+                "http://rem-play-server.yansaan.repl.co/on-app");
 
-            // show snackbar
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          } else {
-            setState(() {
-              SaveLocal.data = HomeModel.fromJson(_result);
-              SaveLocal.isError = false;
-            });
-          }
-        });
+            if (_result == null) {
+              setState(() {
+                SaveLocal.isError = true;
+              });
+              // add message
+              final snackBar = SnackBar(
+                content: const Text("Check your internet connection lol"),
+                action: SnackBarAction(
+                  label: "OK",
+                  // ignore: avoid_returning_null_for_void
+                  onPressed: () => null,
+                ),
+              );
+
+              // show snackbar
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            } else {
+              setState(() {
+                SaveLocal.data = HomeModel.fromJson(_result);
+                SaveLocal.isError = false;
+              });
+            }
+          }),
+    );
   }
 }
